fc485927e3f58ae482656d9e62f8092a1214ccbb
Marcin Floryan <marcin.floryan@gmail.com>
2013-04-17 21:57:18 +0100

    Change vanishing point on mouse click and forget the grid

--- horizon.js
+++ horizon.js
@@ -10,51 +10,49 @@ var HorizonVisualisation = function (canvasElement) {
 
     var grid = [];
 
-    for (var row = 0; row < numOfRows; row++) {
-
-        grid[row] = [];
-
-        var y = canvas.height - ((canvas.height - vanishingPoint.y) / numOfRows) * Math.log((row/2) +1) * 8.5;
-        var totalY = canvas.height - vanishingPoint.y;
-
-        for (var col = 0; col < numOfLines; col++) {
-            grid[row][col] = {y: y};
-
-            var firstRow = grid[0][col];
-            if (row == 0) {
-                firstRow.x = (canvas.width / (numOfLines - 1)) * col;
-            } else {
-                var dy = firstRow.y - y;
-                var totalX = vanishingPoint.x - firstRow.x;
-                grid[row][col].x = firstRow.x + ((totalX * dy) / (totalY));
+    function calculateGrid() {
+        for (var row = 0; row < numOfRows; row++) {
+            grid[row] = [];
+            var y = canvas.height - ((canvas.height - vanishingPoint.y) / numOfRows) * Math.log((row / 2) + 1) * 8.5;
+            var totalY = canvas.height - vanishingPoint.y;
+            for (var col = 0; col < numOfLines; col++) {
+                grid[row][col] = {y: y};
+                var firstRow = grid[0][col];
+                if (row == 0) {
+                    firstRow.x = (canvas.width / (numOfLines - 1)) * col;
+                } else {
+                    var dy = firstRow.y - y;
+                    var totalX = vanishingPoint.x - firstRow.x;
+                    grid[row][col].x = firstRow.x + ((totalX * dy) / (totalY));
+                }
             }
         }
     }
 
-    var drawGrid = function() {
+    var drawGrid = function () {
         context.moveTo(0, vanishingPoint.y);
         context.lineTo(canvas.width, vanishingPoint.y);
 
         context.strokeStyle = "#000";
         context.stroke();
 
-        context.beginPath();
-
-        for (var i = 0; i < numOfLines; i++) {
-            context.moveTo(grid[0][i].x, grid[0][i].y);
-            context.lineTo(vanishingPoint.x, vanishingPoint.y);
-        }
-        context.stroke();
-
-        context.beginPath();
-        context.strokeStyle = "#888";
-
-        for (var i = 0; i < numOfRows; i++) {
-            context.moveTo(grid[i][0].x, grid[i][0].y);
-            context.lineTo(grid[i][numOfLines - 1].x, grid[i][0].y);
-        }
-        context.stroke();
-    }
+//        context.beginPath();
+//
+//        for (var i = 0; i < numOfLines; i++) {
+//            context.moveTo(grid[0][i].x, grid[0][i].y);
+//            context.lineTo(vanishingPoint.x, vanishingPoint.y);
+//        }
+//        context.stroke();
+//
+//        context.beginPath();
+//        context.strokeStyle = "#888";
+//
+//        for (var i = 0; i < numOfRows; i++) {
+//            context.moveTo(grid[i][0].x, grid[i][0].y);
+//            context.lineTo(grid[i][numOfLines - 1].x, grid[i][0].y);
+//        }
+//        context.stroke();
+    };
 
     function drawSquare(col, row, fillStyle) {
         var points = [
@@ -101,13 +99,20 @@ var HorizonVisualisation = function (canvasElement) {
     var squares = [];
 
     var start = function() {
+        calculateGrid();
+        drawGrid();
+
+        canvasElement.addEventListener("click", function(event) {
+            vanishingPoint.x = event.clientX;
+            calculateGrid();
+        });
+
         squares.push(new Square(5));
         setTimeout(step, 200);
     };
 
     var step = function() {
-        context.clearRect(0,0,canvas.width, canvas.height);
-        drawGrid();
+        context.clearRect(0,vanishingPoint.y + 0.5,canvas.width, canvas.height);
 
         var newSquares = [];
 

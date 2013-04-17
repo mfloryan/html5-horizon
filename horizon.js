53ae29f76a663929b65d1e4cf7b4aba733934d07
Marcin Floryan <marcin.floryan@gmail.com>
2013-04-17 09:40:54 +0100

    Separate concerns of calculating and drawing

--- horizon.js
+++ horizon.js
@@ -1,33 +1,19 @@
 var canvas = document.getElementById("horizonCanvas");
-var context = canvas.getContext("2d");
-
-var grid = [];
 
 var vanishingPoint = {x: canvas.width / 2, y: 20 };
-
-context.moveTo(0, 20.5);
-context.lineTo(canvas.width, 20.5);
-
-context.strokeStyle = "#000";
-context.stroke();
 
 var numOfLines = 10;
 var numOfRows = 20;
 
+var grid = [];
 grid[0] = [];
 
 for (var i = 0; i < numOfLines; i++) {
     var x = (canvas.width / (numOfLines - 1)) * i;
     grid[0][i] = {x:x};
-    context.moveTo(x, canvas.height);
-    context.lineTo(vanishingPoint.x, vanishingPoint.y);
 }
-context.stroke();
 
-context.beginPath();
-context.strokeStyle = "#888";
-
-for (var i = 0; i < numOfRows; i++) {
+for (i = 0; i < numOfRows; i++) {
     var y = canvas.height - ((canvas.height - vanishingPoint.y) / numOfRows) * Math.log((i/2) +1) * 8.5;
 
     if (!grid[i]) grid[i] = [];
@@ -40,13 +26,30 @@ for (var i = 0; i < numOfRows; i++) {
             grid[i][j].x = ((canvas.width/2) * (canvas.height - y)) / canvas.height;
         }
     }
-
-    //zmienić X i szerokość
-
-    context.moveTo(i * 21, y);
-    context.lineTo(canvas.width - (i * 21), y);
 }
 
-//zakolorowane kwadraty
+var context = canvas.getContext("2d");
 
+context.moveTo(0, 20.5);
+context.lineTo(canvas.width, 20.5);
+
+context.strokeStyle = "#000";
+context.stroke();
+
+context.beginPath();
+
+for (var i = 0; i < numOfLines; i++) {
+    context.moveTo(grid[0][i].x, canvas.height);
+    context.lineTo(vanishingPoint.x, vanishingPoint.y);
+}
+context.stroke();
+
+context.beginPath();
+context.strokeStyle = "#888";
+
+for (var i = 0; i < numOfRows; i++) {
+
+    context.moveTo(i * 21, grid[i][0].y);
+    context.lineTo(canvas.width - (i * 21), grid[i][0].y);
+}
 context.stroke();
